280821d4fc629ca12ae2a3ad91f1fd8a2db93831
SebastianLai <surregograciano@gmail.com>
2026-09-12 17:35:12 -0500

    create-core-driven-layers

---
+++ src/Modules/Identity/Identity.Api/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using User.Api.Users.RegisterUser;
+
+namespace Identity.Api.Controllers
+{
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public Task<IActionResult> UserRegister(RegisterUserRequest request)
+        {
+
+        }
+    }
+}
